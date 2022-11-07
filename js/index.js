e4e598d786bbe01d8050f884082a60fcd3a64a20
José Gaspar Sánchez García <jg.sanchezgarcia@edu.gva.es>
2022-11-07 06:29:41 +0100

    Añadidos ficheros de prueba

--- js/index.js
+++ js/index.js
@@ -1,9 +1,28 @@
+/**
+ * Funciones para probar el entorno de pruebas
+ * @returns 1
+ */
 function uno()
 {
 	return 1;
 }
-
+/**
+ * Zero
+ * @returns 0
+ */
 function zero()
 {
 	return 0;
 }
+
+function saludar()
+{
+	return "Hola mundo";
+}
+
+// Exportamos la funciones para poder realizar el Testing
+module.exports={
+    uno,
+	zero,
+	saludar,
+};
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,1 +1,36 @@
-/** Banco de pruebas */
+/**
+ * Pruebas unitarias 
+ * @author: José Gaspar Sánchez García.
+ */
+
+const chai = require('chai');
+const mocha = require('mocha');  
+const assert = chai.assert;    // Using Assert style
+const expect = chai.expect;    // Using Expect style
+const should = chai.should();  // Using Should style
+
+const {uno, zero} = require('../js/index');
+
+describe('Banco de pruebas unitarias', () => {
+    describe('Caso de pruebas 1', () => {
+        it('uno() == 1', () => {
+            assert.equal(uno(),1, "Uno");
+        });
+
+        it('zero() == 0', () => {
+            assert.equal(zero(),0, "Zero");
+        });
+    });
+
+    describe('Caso de pruebas 2', () => {
+        it('zero() == 0', () => {
+            assert.equal(zero(),0, "Zero");
+        });
+    });
+    describe('Caso de pruebas 3', () => {
+        it('saludar()', () => {
+            assert.isString();
+        });
+    });
+});
+
